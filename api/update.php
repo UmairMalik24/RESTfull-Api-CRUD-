ae3fe50dfb2bdb7386bd2b73d463a55db5449f08
Umair Malik <umair6336@gmail.com>
2020-08-28 14:55:32 +0500

    Create update.php

---
+++ api/update.php
@@ -0,0 +1,38 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../config/dbs.php';
+  include_once '../../model/post.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Get raw posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // Set ID to update
+  $post->post_id = $data->post_id;
+
+  $post->post_title = $data->post_title;
+  $post->post_content = $data->post_content;
+  $post->post_author = $data->post_author;
+  $post->post_cat_id = $data->post_cat_id;
+
+  // Update post
+  if($post->update()) {
+    echo json_encode(
+      array('message' => 'Post Updated')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Post Not Updated')
+    );
+  }
